132ae178f159428c5e966e1ce5c2855a9535ef84
LioR <lior.greenb@gmail.com>
2019-01-31 12:53:48 +0200

    Commenting out the device placement logs

--- gpu.py
+++ gpu.py
@@ -5,7 +5,7 @@ def set_gpu_session():
     print('Setting gpu session')
     config = tf.ConfigProto(allow_soft_placement = True)
     config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
-    config.log_device_placement = True  # to log device placement (on which device the operation ran)
+    # config.log_device_placement = True  # to log device placement (on which device the operation ran)
                                         # (nothing gets printed in Jupyter, only if you run it standalone)
     sess = tf.Session(config=config)
     set_session(sess)  # set this TensorFlow session as the default session for Keras
