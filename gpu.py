736a2d2c5ab906e477eef7a4ce1ab32d0a3ba409
LioR <lior.greenb@gmail.com>
2019-01-31 12:47:37 +0200

    Allowing soft placement on keras gpu session

--- gpu.py
+++ gpu.py
@@ -3,7 +3,7 @@ from keras.backend.tensorflow_backend import set_session
 
 def set_gpu_session():
     print('Setting gpu session')
-    config = tf.ConfigProto()
+    config = tf.ConfigProto(allow_soft_placement = True)
     config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
     config.log_device_placement = True  # to log device placement (on which device the operation ran)
                                         # (nothing gets printed in Jupyter, only if you run it standalone)
